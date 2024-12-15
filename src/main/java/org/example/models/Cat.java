b3c176ca0ded4258b1f40d9fbe6707051ccd9064
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-12-15 11:48:53 -0600

    models/cat class

---
+++ src/main/java/org/example/models/Cat.java
@@ -0,0 +1,17 @@
+package org.example.models;
+
+public class Cat {
+
+
+
+    private String name;
+    private int age;
+    private Owner owner;
+
+    public Cat(String name, int age, Owner owner) {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+
+    }
+}
