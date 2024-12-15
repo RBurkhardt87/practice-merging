3534da0cb0f177c64881e2564f9a73c584300507
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-12-15 12:10:07 -0600

    testing 2nd commit

--- src/main/java/org/example/models/Owner.java
+++ src/main/java/org/example/models/Owner.java
@@ -6,4 +6,6 @@ public class Owner {
     private String name;
     private int age;
     private int numberOfPets;
+
+    //testing to see if everytime I commit on a branch it makes a pull request, or is it just the first push that creates the branch
 }
